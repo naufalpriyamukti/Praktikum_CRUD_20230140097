84ace20626905161cbb78ab6f3027b77558f394e
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-03-02 10:09:39 +0700

    Menambahkan kelas UserDto dengan atribut id, name, dan age

--- praktikum1/src/main/java/com/deploy/praktikum1/model/dto/UserDto.java
+++ praktikum1/src/main/java/com/deploy/praktikum1/model/dto/UserDto.java
@@ -1,5 +1,10 @@
 package com.deploy.praktikum1.model.dto;
 
+import lombok.Data;
 
-public class UserDto {
+@Data
+public class UserDto{
+    private String id;
+    private String name;
+    private int age;
 }
