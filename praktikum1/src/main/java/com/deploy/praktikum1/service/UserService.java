e29beceef1515f71f64b64156ea3d07ebe850e16
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-03-02 10:33:34 +0700

    Tambah interface UserService untuk mendefinisikan operasi terkait pengguna, termasuk menambahkan, mendapatkan, memperbarui, dan menghapus pengguna.

--- praktikum1/src/main/java/com/deploy/praktikum1/service/UserService.java
+++ praktikum1/src/main/java/com/deploy/praktikum1/service/UserService.java
@@ -6,14 +6,9 @@ import com.deploy.praktikum1.model.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
-
     UserDto AddUser(UserAddRequest request);
-
     List<UserDto> getAllUser();
-
     UserDto getUserById(String id);
-
     UserDto UpdateUser(String id, UserAddRequest request);
-
     void DeleteUser(String id);
 }
